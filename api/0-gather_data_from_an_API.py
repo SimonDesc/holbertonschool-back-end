977a154d2716b5d69e82f9aef184e5d17baa1f3a
SimonDesc <pro.descombes@gmail.com>
2023-08-21 12:28:10 +0200

    feat: refacto for more clarity and maintenability

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,37 +3,31 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: script_name employee_id")
-        sys.exit(1)
+BASE_URL = "https://jsonplaceholder.typicode.com"
+USERS_URL = BASE_URL + "/users/"
+TODOS_URL = BASE_URL + "/todos"
 
-    employee_id = {"id": sys.argv[1]}
 
-    # Get the name of the employee
-    response = requests.get(
-        "https://jsonplaceholder.typicode.com/users/", params=employee_id
-    )
+def get_employee_data(employee_id):
+    """Retrieve employee data."""
+    response = requests.get(USERS_URL, params={"id": employee_id})
     if response.status_code != 200:
-        print("Error fetching employee data")
+        print(f"Error fetching employee data: {response.status_code} {response.text}")
         sys.exit(1)
+    return response.json()[0]
 
-    employee_data = response.json()
-    name_employee = employee_data[0]["name"]
 
-    # Get the total number of tasks
-    user_id = {"userId": sys.argv[1]}
-    task_response = requests.get(
-        "https://jsonplaceholder.typicode.com/todos", params=user_id
-    )
-    if task_response.status_code != 200:
-        print("Error fetching task data")
+def get_tasks_data(user_id):
+    """Retrieve tasks data."""
+    response = requests.get(TODOS_URL, params={"userId": user_id})
+    if response.status_code != 200:
+        print(f"Error fetching task data: {response.status_code} {response.text}")
         sys.exit(1)
+    return response.json()
 
-    tasks_data = task_response.json()
-    number_total_task = len(tasks_data)
 
-    # Get the number of tasks completed
+def get_task_details(tasks_data):
+    """Retrieve task details - completed count and titles."""
     tasks_completed = 0
     tasks_title = []
 
@@ -42,12 +36,38 @@ if __name__ == "__main__":
             tasks_completed += 1
             tasks_title.append(task["title"])
 
-    # print final result
+    return tasks_completed, tasks_title
+
+
+def display_result(name_employee, tasks_completed, number_total_task, tasks_title):
+    """Display the result on the standard output"""
     print(
         "Employee {} is done with tasks({}/{}):".format(
             name_employee, tasks_completed, number_total_task
         )
     )
-
     for title in tasks_title:
         print("\t {}".format(title))
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: script_name employee_id")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    # Retrieve employee related data
+    employee_data = get_employee_data(employee_id)
+    name_employee = employee_data.get("name")
+
+    # Retrieve tasks related data
+    tasks_data = get_tasks_data(employee_id)
+    tasks_completed, tasks_title = get_task_details(tasks_data)
+    number_total_task = len(tasks_data)
+
+    display_result(name_employee, tasks_completed, number_total_task, tasks_title)
+
+
+if __name__ == "__main__":
+    main()
