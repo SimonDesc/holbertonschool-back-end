b1efac11509ebad5f10483376b929d75309d348e
SimonDesc <pro.descombes@gmail.com>
2023-08-21 17:29:16 +0200

    fix: new method for creating the csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -48,7 +48,22 @@ def main():
     # Retrieve tasks related data
     tasks_data = get_tasks_data(employee_id)
 
-    new_list = []
+    # new_list = []
+    # for task in tasks_data:
+    #     inner_list = []
+    #     inner_list = [
+    #         task["userId"],
+    #         name_employee,
+    #         task["completed"],
+    #         task["title"],
+    #     ]
+    #     new_list.append(inner_list)
+
+    # with open(employee_id + ".csv", "w", newline='') as file:
+    #     writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+    #     writer.writerows(new_list)
+
+    list_detailled = []
     for task in tasks_data:
         inner_list = []
         inner_list = [
@@ -57,11 +72,11 @@ def main():
             task["completed"],
             task["title"],
         ]
-        new_list.append(inner_list)
+        list_detailled.append(inner_list)
 
-    with open(employee_id + ".csv", "w", newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerows(new_list)
+    with open(employee_id + ".csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", quoting=csv.QUOTE_ALL)
+        writer.writerows(list_detailled)
 
 
 if __name__ == "__main__":
