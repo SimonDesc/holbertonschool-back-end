56ec0f4b82da34d2f045d970ebb7382ef97fb658
SimonDesc <pro.descombes@gmail.com>
2023-08-28 09:41:52 +0200

    fix

--- python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Write a type-annotated function add that takes a float a and a float b"""
-
-
-def add(a: float, b: float) -> float:
-    """Return the sum of a + b"""
-    return a + b
--- python_variable_annotations/README.md
+++
@@ -1,2 +0,0 @@
-# Python - Variable Annotations
-![# Python - Variable Annotations](https://i.redd.it/y9y25tefi5401.png)
